439654eb34f806527c233d948d0fb0e1a97d305a
Mogens Heller Grabe <mookid8000@gmail.com>
2016-09-09 10:17:21 +0200

    now you live here

---
+++ Rebus.Msmq.Tests/Contracts/MsmqBasicSendReceive.cs
@@ -0,0 +1,8 @@
+﻿using NUnit.Framework;
+using Rebus.Tests.Contracts.Transports;
+
+namespace Rebus.Msmq.Tests.Contracts
+{
+    [TestFixture, Category(Categories.Msmq)]
+    public class MsmqBasicSendReceive : BasicSendReceive<MsmqTransportFactory> { }
+}
---
+++ Rebus.Msmq.Tests/Contracts/MsmqTestManyMessages.cs
@@ -0,0 +1,8 @@
+﻿using NUnit.Framework;
+using Rebus.Tests.Contracts.Transports;
+
+namespace Rebus.Msmq.Tests.Contracts
+{
+    [TestFixture]
+    public class MsmqTestManyMessages : TestManyMessages<MsmqBusFactory> { }
+}
