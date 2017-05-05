503f96abf5f4256406cb6e9767895ab4e1a16c25
Mogens Heller Grabe <mookid8000@gmail.com>
2017-05-05 07:07:36 +0200

    actually fix test

--- Rebus.Msmq.Tests/TestMsmqUtil.cs
+++ Rebus.Msmq.Tests/TestMsmqUtil.cs
@@ -1,21 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using NUnit.Framework;
+using Rebus.Logging;
+using Rebus.Messages;
+using Rebus.Tests.Contracts;
+using Rebus.Transport;
 
 namespace Rebus.Msmq.Tests
 {
     [TestFixture]
-    public class TestMsmqUtil
+    public class TestMsmqUtil : FixtureBase
     {
-        [Test]
-        public void CanGetCount()
+        const string QueueName = "some-randomly-named-queue";
+
+        protected override void SetUp()
         {
-            var path = MsmqUtil.GetPath("global.dealcapture.metadata.input");
+            MsmqUtil.Delete(QueueName);
+        }
+
+        protected override void TearDown()
+        {
+            MsmqUtil.Delete(QueueName);
+        }
+
+        [Test]
+        public async Task CanGetCount()
+        {
+            var path = MsmqUtil.GetPath(QueueName);
+
+            MsmqUtil.EnsureQueueExists(path);
 
             Console.WriteLine($"Checking {path}");
 
-            var count = MsmqUtil.GetCount(path);
+            var countBefore = MsmqUtil.GetCount(path);
 
-            Assert.That(count, Is.EqualTo(29));
+            await SendMessageTo(QueueName);
+            await SendMessageTo(QueueName);
+            await SendMessageTo(QueueName);
+
+            var countAfter = MsmqUtil.GetCount(path);
+
+            Assert.That(countBefore, Is.EqualTo(0));
+            Assert.That(countAfter, Is.EqualTo(3));
+        }
+
+        static async Task SendMessageTo(string queueName)
+        {
+            using (var transport = new MsmqTransport(queueName, new ConsoleLoggerFactory(false)))
+            {
+                using (var scope = new RebusTransactionScope())
+                {
+                    var transportMessage = new TransportMessage(new Dictionary<string, string>(), new byte[0]);
+
+                    await transport.Send(queueName, transportMessage, scope.TransactionContext);
+
+                    await scope.CompleteAsync();
+                }
+            }
         }
     }
 }
